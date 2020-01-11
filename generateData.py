e13dc486f35384b43a0c9ba5fab82614ce5b740b
ezhang7423 <ezhang7423@student.palomar.edu>
2020-01-11 10:23:08 -0800

    create template

---
+++ generateData.py
@@ -0,0 +1,21 @@
+# interests: [
+#     { name: 'Creative Writing', emoji: '🖊️'},
+#     { name: 'Math', emoji: '➕'},
+#     { name: 'Teaching', emoji: '🏫'},
+#     { name: 'Making Friends', emoji: '🎎'},
+#     { name: 'Statistical Analysis', emoji: '📊'},
+#     { name: 'ML/AI', emoji: '🤖'},
+#     { name: 'Video Editing', emoji: '📹'},
+#     { name: 'Scrum/Agile', emoji: '🔄'},
+#     { name: 'Persuasion', emoji: '💬},
+#     { name: 'Coding', emoji: '💻'},
+#     { name: 'Database/SQL', emoji: '📋'},
+#     { name: 'Listening', emoji: '👂'},
+#     { name: 'Technical Writing', emoji: '📚'},
+#     { name: 'Troubleshooting', emoji: '💡'},
+#     { name: 'Negotiation', emoji: '🤝'},
+# ]
+
+cT = [0, 0, 1, 0, 0, 7, 1, 0, 5, 5, 0, 4, 2, 10, 5, 6, 8, 3, 3]
+bT = [0, 0, 0, 1, 3, 5, 6, 9, 4, 0, 0, 9, 7, 0, 0, 9, 2, 4, 10]
+print(len(cT))
