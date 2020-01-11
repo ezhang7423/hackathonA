5f9343c8ffeda5dcc22e6f96adbd446d7832041e
ezhang7423 <ezhang7423@student.palomar.edu>
2020-01-11 10:54:16 -0800

    got the data, add the labels next

--- generateData.py
+++ generateData.py
@@ -15,7 +15,47 @@
 #     { name: 'Troubleshooting', emoji: '💡'},
 #     { name: 'Negotiation', emoji: '🤝'},
 # ]
+import random
 
 cT = [0, 0, 1, 0, 0, 7, 1, 0, 5, 5, 0, 4, 2, 10, 5, 6, 8, 3, 3]
 bT = [0, 0, 0, 1, 3, 5, 6, 9, 4, 0, 0, 9, 7, 0, 0, 9, 2, 4, 10]
-print(len(cT))
+
+
+random.seed(42)
+# with open("data.txt", 'w') as out:
+
+
+def randomWeight(tmp):
+    rand = (random.randint(0, 1000) - 500) / 1000
+    tmp = round(tmp * (rand + 1), 2)
+    if (tmp > 10):
+        tmp = 10
+    if (tmp < 0):
+        tmp = 0
+    return tmp
+
+
+def generate10000(type):
+    coders = []
+    for x in range(10000):
+        coders.append(type.copy())
+        for y in range(15):
+            # generate skills
+            tmp = cT[y+4]
+            tmp = tmp + .5
+            coders[x][y+4] = randomWeight(tmp)
+            # generate interests
+            tmp = 10 - cT[y+4]
+            coders[x].append(randomWeight(tmp))
+    return coders
+
+
+def makeString(liste):
+    return '\n'.join(str(e) for e in liste)
+
+
+with open("data.out", 'w') as out:
+    coders = generate10000(cT)
+    bizppl = generate10000(bT)
+    out.write(makeString(coders))
+    out.write(makeString(bizppl))
