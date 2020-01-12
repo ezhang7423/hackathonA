f3b69ad348769b750d5d71186fd52650869aa47e
ezhang7423 <ezhang7423@student.palomar.edu>
2020-01-11 23:18:28 -0800

    completely fixed

--- index.py
+++ index.py
@@ -24,9 +24,13 @@ def sConcat(arr1, arr2):
 # need user vectors of users and filter list
 # returns sorted list of user vectors
 def cnnapi():
+    print('received request')
     rjson = request.get_json()
     uid = rjson['u']
     predictionArr = rjson['o']
+    print(uid, predictionArr)
+    print('starting prediction')
+    print(len(predictionArr[0]))
     model = models.load_model('model.hdf5')
     fP = []
     for x in range(len(predictionArr)):
@@ -34,12 +38,18 @@ def cnnapi():
     fP = np.asarray(fP).astype('float32')
     rankedL = model.predict(fP)
     returnD = {}
+    print(rankedL)
     for x in range(len(rankedL)):
-        returnD[predictionArr[x]] = rankedL[x]
+        returnD[x] = rankedL[x][0]
     returnD = {k: v for k, v in sorted(
-        returnD.items(), key=lambda item: item[1])}
-    return returnD
+        returnD.items(), key=lambda item: item[1], reverse=True)}
+    print(returnD)
+    finalAns = [k for k, v in returnD.items()]
+    return json.dumps(str(finalAns))
 
 
 if __name__ == "__main__":
     app.run()
+
+
+# curl -v -d '{"u": [0, 0, 0, 1, 3.21, 4.79, 6.1, 7.44, 4.61, 0.45, 0.55, 10, 7.29, 0.58, 0.68, 10, 3.17, 5.4, 7.86, 7.56, 4.86, 3.26, 2.2, 5.49, 9.63, 9.1, 0.0, 2.79, 9.91, 6.66, 0.0, 8.66, 2.66, 2.07], "o": [[0, 0, 0, 1, 3.33, 5.17, 7.9, 10, 5.02, 0.29, 0.36, 7.99, 7.39, 0.57, 0.48, 6.57, 2.39, 5.01, 8.88, 3.96, 4.75, 2.25, 0.0, 6.01, 7.69, 10, 1.85, 3.15, 9.91, 9.7, 3.26, 5.72, 4.85, 1.21], [0, 0, 1, 0, 0.34, 7.86, 1.71, 0.4, 4.39, 4.9, 0.52, 4.36, 1.8, 7.44, 5.45, 5.2, 10, 2.71, 4.01, 8.84, 1.3, 7.48, 10, 6.9, 7.11, 9.65, 7.34, 8.82, 2.99, 7.12, 5.48, 0.0, 6.7, 4.29]]}' -H "Content-Type: application/json" localhost:5000/cnnapi
